9d1334ffa6ec262274a71a98b58e17f2f2f37bc3
Kryakla0578 <167692954+Kryakla0578@users.noreply.github.com>
2024-06-02 18:33:53 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+a = '7887'
+print (len(a))
+
+first = 56
+second = 65
+summa = first + second
+diff = first - second
+print (summa)
+print (diff)
+
+third = 5665
+mean = (first + second + third)/3
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,', ',first_string)
+
+a = 5
+b = 6
+c = 7
+f = (a * b) + (a * c)
+print(f)
